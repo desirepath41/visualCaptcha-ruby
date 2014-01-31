60451ea71e3c912439c21458b59a993345467dfd
Miles D Goodings <miles@clevertech.biz>
2014-01-31 23:35:36 +1000

    add /try route for captcha testing

--- app.rb
+++ app.rb
@@ -5,6 +5,7 @@ require 'visual_captcha'
 
 class App < Sinatra::Base
   set :sessions, true
+  set :public_folder, 'public'
 
   before do
     @session = VisualCaptcha::Session.new session
@@ -15,6 +16,10 @@ class App < Sinatra::Base
 
   after do
     headers @headers
+  end
+
+  get '/' do
+    send_file File.join( 'public', 'index.html' )
   end
 
   get '/start/:how_many' do
@@ -45,6 +50,37 @@ class App < Sinatra::Base
     else
       not_found
     end
+  end
+
+  post '/try' do
+    captcha = VisualCaptcha::Captcha.new @session
+    frontend_data = captcha.frontend_data()
+
+    if frontend_data.nil?
+      redirect_path = '/?status=noCaptcha'
+    else
+      # If an image field name was submitted, try to validate it
+      if ( image_answer = params[ frontend_data[ 'imageFieldName' ] ] )
+        if captcha.validate_image image_answer
+          redirect_path = '/?status=validImage'
+        else
+          redirect_path = '/?status=failedImage'
+        end
+      elsif ( audio_answer = params[ frontend_data[ 'audioFieldName' ] ] )
+        if captcha.validate_audio audio_answer.downcase
+          redirect_path = '/?status=validAudio'
+        else
+          redirect_path = '/?status=failedAudio'
+        end
+      else
+        redirect_path = '/?status=failedPost'
+      end
+
+      how_many = captcha.selected_images().length
+      captcha.generate( how_many )
+    end
+
+    redirect redirect_path
   end
 
   run! if app_file == $0

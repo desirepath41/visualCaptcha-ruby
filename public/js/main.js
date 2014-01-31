9c33b0afa9f71cda4b0d3ff5c79f1fe48437f0e8
Alexander Bykhov <another.side.of.my.soul@gmail.com>
2014-02-01 00:36:03 +0400

    Re #22: Implement pretty demo design into the Ruby demo

--- public/js/main.js
+++ public/js/main.js
@@ -13,12 +13,36 @@
                 }
             };
 
-            $scope.alertCaptchaData = function() {
-                if ( $scope.captcha ) {
-                    alert( JSON.stringify( $scope.captcha.getCaptchaData(), null, '\t' ) );
+            $scope.isVisualCaptchaFilled = function() {
+                if ( $scope.captcha.getCaptchaData().valid ) {
+                    window.alert( 'visualCaptcha is filled!' );
                 } else {
-                    alert( 'Captcha not set' );
+                    window.alert( 'visualCaptcha is NOT filled!' );
                 }
+            };
+
+
+            var queryString = window.location.search;
+            // Show success/error messages
+            $scope.status = null;
+            if ( queryString.indexOf('status=noCaptcha') !== -1 ) {
+                $scope.valid = false;
+                $scope.status = 'visualCaptcha was not started!';
+            } else if ( queryString.indexOf('status=validImage') !== -1 ) {
+                $scope.valid = true;
+                $scope.status = 'Image was valid!';
+            } else if ( queryString.indexOf('status=failedImage') !== -1 ) {
+                $scope.valid = false;
+                $scope.status = 'Image was NOT valid!';
+            } else if ( queryString.indexOf('status=validAudio') !== -1 ) {
+                $scope.valid = true;
+                $scope.status = 'Accessibility answer was valid!';
+            } else if ( queryString.indexOf('status=failedAudio') !== -1 ) {
+                $scope.valid = false;
+                $scope.status = 'Accessibility answer was NOT valid!';
+            } else if ( queryString.indexOf('status=failedPost') !== -1 ) {
+                $scope.valid = false;
+                $scope.status = 'No visualCaptcha answer was given!';
             }
         } );
 }( window, angular ) );
